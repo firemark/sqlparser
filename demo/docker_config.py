0c50bbd560478fddf841e6367554ef287650f494
Marek Piechula <m.piechula@clearcode.cc>
2017-07-26 16:54:03 +0200

    bugfix and add special vars to demo

--- demo/docker_config.py
+++ demo/docker_config.py
@@ -6,6 +6,7 @@ from sqlalchemy.sql.expression import table
 from sqlparser.tables.sql import SqlTable
 from sqlparser.table_manager import TableManager
 from sqlparser.tables.mongo import MongoTable
+from sqlparser.tables.utils import special_vars
 
 mongo = MongoClient('localhost', 6961)
 engine = create_engine('postgresql://demo:demo@0:6960/demo')
@@ -16,8 +17,16 @@ manager = TableManager()
 class AirlineSqlTable(SqlTable):
     TABLE = table('airline')
     ENGINE = engine
+    SPECIAL_VARS = special_vars(
+        HOUR=60,
+        PORTLAND='Portland, OR'
+    )
 
 
 @manager.register('mongo_airline')
 class AirlineMongoTable(MongoTable):
     TABLE = mongo.demo.airline
+    SPECIAL_VARS = special_vars(
+        HOUR=60,
+        PORTLAND='Portland, OR'
+    )
